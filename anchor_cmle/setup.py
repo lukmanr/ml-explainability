3d1475046c2283984ab8e4377a99e23f22036427
Alan Krumholz <akrumholz@google.com>
2019-05-01 17:16:50 +0000

    anchor notebook and python library

---
+++ anchor_cmle/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='anchor_cmle',
+      version='0.1',
+      description='Use anchor explainability on models hosted in CMLE',
+      packages=['anchor_cmle'],
+      zip_safe=False)
