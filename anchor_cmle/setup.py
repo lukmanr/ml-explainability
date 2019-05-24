e718538367a4b548c6d4d4eb4bf981791cd8ebe1
Alan Krumholz <akrumholz@google.com>
2019-05-24 10:34:21 -0700

    rebranding

--- anchor_cmle/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='anchor_cmle',
-      version='0.1',
-      description='Use anchor explainability on models hosted in CMLE',
-      packages=['anchor_cmle'],
-      zip_safe=False)
